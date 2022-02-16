d30c4755d6d562bae8e45d4d233e2e3adc9b2844
Damien <zara.damien21@gmail.com>
2022-02-16 15:44:45 +0100

    Setup test fail fix wip

--- upload/package-files/Sources/OneallSocialLogin.sdk.php
+++ upload/package-files/Sources/OneallSocialLogin.sdk.php
@@ -28,7 +28,7 @@ if (!defined('SMF'))
 }
 
 // OneAll Social Login Version
-define('OASL_VERSION', '3.8');
+define('OASL_VERSION', '4.3.0');
 define('OASL_USER_AGENT', 'SocialLogin/' . OASL_VERSION . ' SMF/2.x.x (+http://www.oneall.com/)');
 
 /**
